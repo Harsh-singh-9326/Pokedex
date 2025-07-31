dc12daaaf172e37e4ddf74ed0cb2f89edcbd94f5
Harsh-singh-9326 <harshS9326@gmail.com>
2025-07-31 22:20:51 +0530

    add prevor next button logic

--- src/assets/components/pokemonlist/pokemonlist.jsx
+++ src/assets/components/pokemonlist/pokemonlist.jsx
@@ -1,51 +1,66 @@
-import axios from "axios"
-import { useEffect, useState } from "react"
+import "./pokemonlist.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
 import Pokemon from "../pokemon/pokemon";
 
+export default function Pokemonlist() {
+  const [pokemonlist, setpokemonlist] = useState([]);
+  const [isloading, setisloading] = useState(true);
 
-export default function Pokemonlist (){
-    const [ pokemonlist,setpokemonlist ] = useState([]);
-    const [isloading , setisloading] = useState(true);
+  const [PokemonURl, setPokemonURl]=useState("https://pokeapi.co/api/v2/pokemon")
 
-    async function  getpokemon() {
-        const response =  await axios.get("https://pokeapi.co/api/v2/pokemon")
-        const PokemonResult = response.data.results 
-        const PokemonResultPromises =await PokemonResult.map((pokemon)=>axios.get(pokemon.url))
-        const PokemonData = await axios.all(PokemonResultPromises) 
-        console.log(PokemonData)
-        const res = PokemonData.map((pokedata)=>{
-            const pokemon = pokedata.data
-            return{
-            id :pokemon.id,    
-            name :pokemon.name,
-            image: pokemon.sprites.other["official-artwork"].front_default,
-            type :pokemon.types.map((t)=> t.type.name).join(", ")
-            
-        }
-        })
-        console.log(res)
-        setpokemonlist(res)
+  const [prevUrl, setprevUrl] = useState("")
+  const [NextUrl, setNextUrl] = useState("")
 
-        setisloading(false)
 
-    }
-    useEffect(()=>{
-        getpokemon()
-         
+  async function getpokemon() {
+    const response = await axios.get(PokemonURl);
+    const PokemonResult = response.data.results;
+     setNextUrl(response.data.next)
+     setprevUrl(response.data.previous)
+    const PokemonResultPromises = await PokemonResult.map((pokemon) =>
+      axios.get(pokemon.url)
+    );
+    const PokemonData = await axios.all(PokemonResultPromises);
+    console.log(PokemonData);
+    const res = PokemonData.map((pokedata) => {
+      const pokemon = pokedata.data;
+      return {
+        id: pokemon.id,
+        name: pokemon.name,
+        image: pokemon.sprites.other["official-artwork"].front_default,
+        type: pokemon.types.map((t) => t.type.name).join(", "),
+      };
+    });
+    console.log(res);
 
-    },[])
+    setpokemonlist(res);
 
-    return (
-        <div>
-         <div>Pokemon list
-         
-         {isloading?"loading.....":pokemonlist.map((p)=><Pokemon name={p.name} image={p.image} type={p.type} key={p.id} />) }
-         </div>
-         <div>
-            <button>Next</button>
-            <button>Prev</button>
-         </div>
+    setisloading(false);
+  }
+  useEffect(() => {
+    getpokemon();
+  }, [PokemonURl]);
 
-        </div>
-    )
+  return (
+    <div>
+      <div>
+        <h2 className="heading">Pokemon List</h2>
+        {isloading
+          ? "loading....."
+          : pokemonlist.map((p) => (
+              <Pokemon name={p.name} image={p.image} type={p.type} key={p.id} />
+            ))}
+      </div>
+      <div className="button-group">
+        <button disabled={!prevUrl} onClick={()=>{if (prevUrl) {
+      setPokemonURl(prevUrl);
+      window.scrollTo({ top: 0, behavior: "smooth" }); }}}>Prev</button>
+        <button disabled={!NextUrl} onClick={()=>{if (NextUrl) {
+      setPokemonURl(NextUrl);
+      window.scrollTo({ top: 0, behavior: "smooth" }); }}}>Next</button>
+      </div>
+    </div>
+  );
 }
+
